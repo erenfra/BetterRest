62f4c5535cb2f41b50ee84dc955c531d0f40619c
Renato Oliveira Fraga <fraga@Renatos-iMac.local>
2021-05-24 21:37:20 -0500

    first commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Renato Oliveira Fraga on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
